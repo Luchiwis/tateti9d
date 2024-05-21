ca48d5dca0c761c98d425ac853667ba86b3211ef
lucio <creppenderman@gmail.com>
2024-05-21 00:20:39 -0300

    changed project folder system to introduce backend

---
+++ front/src/Grid.jsx
@@ -0,0 +1,111 @@
+
+export function Grid() {
+    return (
+        <>
+            <div className="flex justify-center">
+
+                <div id="grid" className="mainGrid m-5">
+                    <div className="universo tl">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo tm">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo tr">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo ml">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo mm">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo mr">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo bl">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo bm">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                    <div className="universo br">
+                        <div className="division tl"></div>
+                        <div className="division tm"></div>
+                        <div className="division tr"></div>
+                        <div className="division ml"></div>
+                        <div className="division mm"></div>
+                        <div className="division mr"></div>
+                        <div className="division bl"></div>
+                        <div className="division bm"></div>
+                        <div className="division br"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
